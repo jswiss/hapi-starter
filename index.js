20d827d89b80746e22935054d53be438730adcf0
Joshua Swiss <swissjoshua@gmail.com>
2017-04-07 10:20:55 +0300

    joi is cool

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Hapi = require('hapi');
+const Joi = require('joi');
 const server = new Hapi.Server();
+
 
 server.connection({
   port: 3000
@@ -7,14 +9,21 @@ server.connection({
 
 server.route({
   method: 'GET',
-  path: '/{josh*}', // this is how you capture route params in Hapi
-  handler: function (req, reply) {
-    console.log(req.params);
-    reply(`Hello ${req.params.josh}!`); // reply with text
+  path: '/{yourname*}', // this is how you capture route params in Hapi
+  config: {
+    validate: {
+      params: {
+        yourname: Joi.string().min(2).max(40).alphanum().required()
+      }
+    },
+    handler: function (req, reply) {
+      console.log(req.params);
+      reply(`Hello ${req.params.yourname}!`); // reply with text
+    },
   }
 });
 
 
 server.start(() => { //will hstart Hapi server on your port
-  console.log(`Now visit: http://localhost:${server.info.port}/josh`);
+  console.log(`Now visit: http://localhost:${server.info.port}/yourname`);
 });
